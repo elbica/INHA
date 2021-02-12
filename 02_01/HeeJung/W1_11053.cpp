6f318933d2f4912840a41e0781eb799ca7ae4c7b
hwangheejung <dap072055@naver.com>
2021-02-13 00:24:17 +0900

    W1

--- 02_01/HeeJung/W1_11053.cpp
+++ 02_01/HeeJung/W1_11053.cpp
@@ -1,25 +1,25 @@
 #include<iostream>
 using namespace std;
+
 int max(int x, int y) {
-	if (x > y) 
+	if (x > y)
 		return x;
 	else
 		return y;
 }
 int main() {
 	int n;
-	cin >> n;
 	int arr[1001];
 	int dp[1001];
+	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
 	int an = 0;
 	for (int i = 1; i <= n; i++) {
 		dp[i] = 1;
-		for (int j = i - 1; j >= 1; j--) {
-			if (arr[i]< arr[j])
-			{
+		for (int j = i-1; j >= 1; j--) {
+			if (arr[i] > arr[j]) {
 				dp[i] = max(dp[i], dp[j] + 1);
 			}
 		}
